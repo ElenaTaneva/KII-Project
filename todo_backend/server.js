da83f65d6f19b3cd5a343d20cb43a790ca521fdf
et27 <elena.taneva@students.finki.ukim.mk>
2026-06-23 19:00:32 +0200

    Add Docker Compose with mongo, backend and frontend services

--- todo_backend/server.js
+++ todo_backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TODO',
+mongoose.connect('mongodb://mongo:27017/TODO',
     console.log('MongoDB connected')
 )
 
